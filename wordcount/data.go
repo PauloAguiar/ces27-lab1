a8b962970399c5996bd32996455bad73715331cb
Paulo Araujo <phaguiardm@gmail.com>
2016-08-24 22:09:31 -0300

    Remove directional channels.

--- wordcount/data.go
+++ wordcount/data.go
@@ -21,9 +21,7 @@ const (
 // the mapreduce framework through the one-way channel. It'll buffer data up to
 // MAP_BUFFER_SIZE (files smaller than chunkSize) and resume loading them
 // after they are read on the other side of the channle (in the mapreduce package)
-//
-// Don't understand the type '<-chan []byte'? Please visit: https://gobyexample.com/channel-directions
-func fanInData(numFiles int) <-chan []byte {
+func fanInData(numFiles int) chan []byte {
 	var (
 		err    error
 		input  chan []byte
@@ -50,9 +48,7 @@ func fanInData(numFiles int) <-chan []byte {
 // fanOutData will run a goroutine that receive data on the one-way channel and will
 // proceed to store it in their final destination. The data will come out after the
 // reduce phase of the mapreduce model.
-//
-// Don't understand the type 'chan<- []mapreduce.KeyValue'? Please visit: https://gobyexample.com/channel-directions
-func fanOutData() (output chan<- []mapreduce.KeyValue, done chan bool) {
+func fanOutData() (output chan []mapreduce.KeyValue, done chan bool) {
 	var (
 		err           error
 		file          *os.File
