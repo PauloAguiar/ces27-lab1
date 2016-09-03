d7acef9cc57126150e29a399161c99640bd2c295
davilimap <davilimap@gmail.com>
2016-09-03 18:01:28 -0300

    Map and Reduce functions implemented

--- wordcount/wordcount.go
+++ wordcount/wordcount.go
@@ -1,31 +1,55 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
 	"github.com/pauloaguiar/ces27-lab1/mapreduce"
 	"hash/fnv"
+	"unicode"
+	"unicode/utf8"
+	"strings"
+	"strconv"
 )
 
 // mapFunc is called for each array of bytes read from the splitted files. For wordcount
 // it should convert it into an array and parses it into an array of KeyValue that have
 // all the words in the input.
 func mapFunc(input []byte) (result []mapreduce.KeyValue) {
-	// 	Pay attention! We are getting an array of bytes. Cast it to string.
-	//
-	// 	To decide if a character is a delimiter of a word, use the following check:
-	//		!unicode.IsLetter(c) && !unicode.IsNumber(c)
-	//
-	//	Map should also make words lower cased:
-	//		strings.ToLower(string)
-	//
-	// IMPORTANT! The cast 'string(5)' won't return the character '5'.
-	// 		If you want to convert to and from string types, use the package 'strconv':
-	// 			strconv.Itoa(5) // = "5"
-	//			strconv.Atoi("5") // = 5
 
-	fmt.Printf("Input Length: %v\n", len(input))
-	
+	//Create an empty array of KeyValues
 	result = make([]mapreduce.KeyValue, 0)
+
+	//This stores each individual word as it is being constructed from the input
+	word := make([]byte, 0)
+
+	//The following code is based on the one found on: https://golang.org/pkg/unicode/utf8/#DecodeRune
+
+	//We work with a slice to facilitade manipulations
+	b := input[:]
+
+
+	for len(b) > 0 {
+		//Gets the first rune in the input and the size
+		r, size := utf8.DecodeRune(b)
+
+		//If it's a letter or number, add it to the word currently being assembled
+		if(unicode.IsLetter(r) || unicode.IsNumber(r)) {
+			word = append(word, b[:size]...)
+		} else { //If the character is not a letter or number, the word is finished
+			//If the word exists, add it to the result and clean the word buffer
+			if len(word) > 0 {
+				result = append(result, mapreduce.KeyValue{strings.ToLower(string(word)), "1"})
+				word = word[:0]
+			}
+		}
+		//Move to the next rune
+		b = b[size:]
+	}
+
+	//There may still be a word in the buffer when we reach the end. If there is, add it as well
+	if len(word) > 0 {
+		result = append(result, mapreduce.KeyValue{strings.ToLower(string(word)), "1"})
+	}
+
 	return result
 }
 
@@ -46,10 +70,24 @@ func reduceFunc(input []mapreduce.KeyValue) (result []mapreduce.KeyValue) {
 	// 	It's also possible to receive a non-numeric value (i.e. "+"). You can check the
 	// 	error returned by Atoi and if it's not 'nil', use 1 as the value.
 
-	/////////////////////////
-	// YOUR CODE GOES HERE //
-	/////////////////////////
+	
+
+	aux := make(map[string]int)
+
+	for _, v := range input {
+		if i, e := strconv.Atoi(v.Value); e != nil {
+			aux[v.Key]++
+		} else {
+			aux[v.Key] += i
+		}
+	}
+
 	result = make([]mapreduce.KeyValue, 0)
+
+	for k, v := range aux {
+		result = append(result, mapreduce.KeyValue{k, strconv.Itoa(v)})
+	}
+
 	return result
 }
 
