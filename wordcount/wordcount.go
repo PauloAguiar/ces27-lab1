6c3205d8dbeff2571e45a1b5a79dd1e27685aa91
MexicanoT18 <lucas.fra.oli18@gmail.com>
2017-10-30 00:54:57 -0200

    lab pronto

--- wordcount/wordcount.go
+++ wordcount/wordcount.go
@@ -3,6 +3,9 @@ package main
 import (
 	"github.com/pauloaguiar/ces27-lab1/mapreduce"
 	"hash/fnv"
+	"unicode"
+	"strings"
+	"strconv"
 )
 
 // mapFunc is called for each array of bytes read from the splitted files. For wordcount
@@ -22,11 +25,34 @@ func mapFunc(input []byte) (result []mapreduce.KeyValue) {
 	// 			strconv.Itoa(5) // = "5"
 	//			strconv.Atoi("5") // = 5
 
-	/////////////////////////
-	// YOUR CODE GOES HERE //
-	/////////////////////////
-	result = make([]mapreduce.KeyValue, 0)
-	return result
+	last := 0;
+	result = make([]mapreduce.KeyValue, len(input));
+	cnt := 0;
+
+	for i := 0; i <= len(input); i++ {
+
+		var c rune;
+		if i < len(input) {
+			c = rune(input[i]);
+		} else {
+			c = ' ';
+		}
+
+		if !unicode.IsLetter(c) && !unicode.IsNumber(c) {
+
+			if i > last {
+				var kv mapreduce.KeyValue;
+				kv.Key = strings.ToLower(string(input[last:i]));
+				kv.Value = "1";
+				result[cnt] = kv;
+				cnt++;
+			}
+
+			last = i+1;
+		}
+	}
+
+	return result[0:cnt];
 }
 
 // reduceFunc is called for each merged array of KeyValue resulted from all map jobs.
@@ -46,11 +72,33 @@ func reduceFunc(input []mapreduce.KeyValue) (result []mapreduce.KeyValue) {
 	// 	It's also possible to receive a non-numeric value (i.e. "+"). You can check the
 	// 	error returned by Atoi and if it's not 'nil', use 1 as the value.
 
-	/////////////////////////
-	// YOUR CODE GOES HERE //
-	/////////////////////////
-	result = make([]mapreduce.KeyValue, 0)
-	return result
+	var app map[string]int;
+	app = make(map[string]int);
+	
+	for i := 0; i < len(input); i++ {
+		var kv mapreduce.KeyValue;
+		kv = input[i];
+
+		var cnt int;
+		if !unicode.IsNumber(rune(kv.Value[0])) {
+			cnt = 1;
+		} else {
+			cnt, _ = strconv.Atoi(kv.Value);
+		}
+
+		app[kv.Key] = app[kv.Key] + cnt;
+	}
+
+	result = make([]mapreduce.KeyValue, len(app));
+
+	i := 0;
+	for key, cnt := range app {
+		result[i].Key = key;
+		result[i].Value = strconv.Itoa(cnt);
+		i++;
+	}
+	
+	return result;
 }
 
 // shuffleFunc will shuffle map job results into different job tasks. It should assert that
